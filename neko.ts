d9bb375d26e6550923cd3f5b293879afa9e51e89
ShellTear <62976449+SheIITear@users.noreply.github.com>
2024-10-12 23:31:39 +0300

    Update neko.ts

--- neko.ts
+++ neko.ts
@@ -1,9 +1,9 @@
 const getNeko = async () => {
     try {
-    const response = await fetch('https://nekos.best/api/v2/neko')
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?format=json')
     const json = await response.json() as any
-    if (!json.results[0].url) throw ('req failed neko')
-    return json.results[0].url
+    if (!json[0].url) throw ('req failed neko')
+    return json[0].url
     }
     catch (e) {
         return undefined
